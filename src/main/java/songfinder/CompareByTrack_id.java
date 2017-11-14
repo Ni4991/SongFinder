2e61f16f46a930ef8b62287440797443f5607d00
Nina Luo <30792189+Ni4991@users.noreply.github.com>
2017-11-13 23:28:51 -0800

    Update CompareByTrack_id.java

--- src/main/java/songfinder/CompareByTrack_id.java
+++ src/main/java/songfinder/CompareByTrack_id.java
@@ -2,6 +2,11 @@ package songfinder;
 
 import java.util.Comparator;
 
+/**
+ * A class to sort by track_id.
+ * @author nina luo
+ *
+ */
 public class CompareByTrack_id implements Comparator<SongInfo> {
 	@Override
 	public int compare(SongInfo s1, SongInfo s2) {
