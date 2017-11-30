905e454fae6e616fb47937785b5bdf363b055e84
Nina Luo <30792189+Ni4991@users.noreply.github.com>
2017-11-30 01:05:26 -0800

    pro3

--- src/main/java/servlets/ArtistInfo.java
+++ src/main/java/servlets/ArtistInfo.java
@@ -1,5 +1,10 @@
 package servlets;
 
+/**
+ * a class to store info for a single artist.
+ * @author nluo
+ *
+ */
 public class ArtistInfo{
 	
 	private String name, bio, image;
@@ -13,25 +18,43 @@ public class ArtistInfo{
 		this.playcount = playcount;
 	}
 	
+	/**
+	 * getter for artist's name.
+	 * @return
+	 */
 	public String getName() {
 		return name;
 	}
 	
+	/**
+	 * getter for artist's image(small).
+	 * @return
+	 */
 	public String getImage() {
 		return image;
 	}
 	
+	/**
+	 * getter for artist's bio.
+	 * @return
+	 */
 	public String getBio() {
 		return bio;
 	}
 	
+	/**
+	 * getter for artist's number of listeners.
+	 * @return
+	 */
 	public int getListeners() {
 		return listeners;
 	}
 	
+	/**
+	 * getter for artist's play count.
+	 * @return
+	 */
 	public int getPlaycount() {
 		return playcount;
 	}
-	
-
 }
