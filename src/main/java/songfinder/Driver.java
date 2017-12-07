fb60c66f69dd67da12f97560f63577758a0307d2
Nina Luo <30792189+Ni4991@users.noreply.github.com>
2017-12-06 20:10:27 -0800

    rel2

--- src/main/java/songfinder/Driver.java
+++ src/main/java/songfinder/Driver.java
@@ -1,13 +1,9 @@
-package songLibrary;
-
-import java.io.File;
-import java.io.FileNotFoundException;
+package songfinder;
 
 import org.json.JSONException;
 
 import general.LibraryBuilder;
 import general.ParseArgs;
-import socket.HTTPFetcher;
 
 /**
  * main class for SongFinder lab and projects.
@@ -31,15 +27,6 @@ public class Driver {
 					,pa.getOutputpath(), pa.getArtistsToSearch(), pa.getTitlesToSearch(), 
 					pa.getTagsToSearch(), pa.getDoSearch(), pa.getSearchOutputpath());
 			lb.build();	//TODO: uncomment before submit
-//			String punk = HTTPFetcher.download("ws.audioscrobbler.com", "/2.0/?method=artist.getinfo"
-//					+ "&artist=Punk%27d+Royal&api_key=9162bc3f7439ff3d4258613b75d37287&format=json");
-//			System.out.print(punk);//$.artist.bio.summary
-//			String robin = HTTPFetcher.download("ws.audioscrobbler.com", "/2.0/?method=artist.getinfo"
-//					+ "&artist=Robin+Frederick&api_key=9162bc3f7439ff3d4258613b75d37287&format=json");
-//			System.out.print(robin);//$.artist.bio.content
-//			String walter = HTTPFetcher.download("ws.audioscrobbler.com", "/2.0/?method=artist.getinfo"
-//					+ "&artist=Walter+Trout&api_key=9162bc3f7439ff3d4258613b75d37287&format=json");
-//			System.out.print(walter);//$.artist.bio.content
 		} catch (JSONException e) {
 			e.printStackTrace();
 		}	
